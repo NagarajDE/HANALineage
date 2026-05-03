f8ca2d7aeabae1dba829e74b0362cf0a28e9a65d
ngumatima1-illumina <ngumatimath1@illumina.com>
2026-05-03 06:38:51 -0700

    update

--- view_list_query.sql
+++ view_list_query.sql
@@ -5,8 +5,13 @@ select
     object_name
 from "_SYS_REPO"."ACTIVE_OBJECT"
 where object_suffix  = 'calculationview' and
- (package_id like 'ILMN.%P2D%' or package_id like 'ILMN.%P2P%')
-  and object_name not like '%_QV'
+ (package_id not like '%DEV%' and  package_id not like '%ILMN-LIVEVIEWS%')
+  and object_name  like '%_QV'
 )
 
-select * from q1 where package_id||object_name not in ( select packagename||viewname from "NGUMATIMA1"."LINEAGE" )
+select * from q1 
+where package_id||object_name not in ( select packagename||viewname from "NGUMATIMA1"."LINEAGE" )
+and package_id like '%ILMN%'
+
+
+
